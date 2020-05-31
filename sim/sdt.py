a67e685c825ed649a13a510aca3cc035839f1963
Jonas Nelle <jonas.a.nelle@gmail.com>
2020-05-31 14:20:44 +0200

    wrap gain as a fn of parameters, add threshold

--- sim/sdt.py
+++ sim/sdt.py
@@ -3,14 +3,26 @@ import matplotlib.pyplot as plt
 
 # Signal-detection expected gain for analytic solution
 # based on the probability of Vh_1 coming from V as opposed to Vp
-def SDT_gain(V=1, Vp=0, N=20, sigma=1, plot=True, get_gain=False):
+def SDT_gain(
+    V=1,
+    Vp=0,
+    N=20,
+    sigma=1,
+    threshold=0.25,
+    cost_eval=0.1,
+    get_gain=False,
+    get_dynamic=False,
+):
 
     Vh_1_list = np.random.normal(V, sigma, N)  # draw N Vh_1 from V
     Vh_2_list = np.random.normal(Vp, sigma, N)  # draw N Vh_2 from V'
 
-    probs = np.ones(N)  # probability that Vh_1 is drawn from V (as opposed to V')
-    gain = np.ones(N)  # expected gain from evaluating A_1
-    switch = np.zeros(N)  # whether Vh_1 < Vh_2
+    probs = np.zeros(N)  # probability that Vh_1 is drawn from V (as opposed to V')
+    gain = np.zeros(N)  # expected gain from evaluating A_1
+    switch = np.zeros(N, dtype="int")  # whether Vh_1 < Vh_2
+    default = np.zeros(N)  # value if choose action A_1 always
+    dynamic = np.zeros(N)  # value if choose dynamically to evaluate or not
+    did_eval = np.zeros(N, dtype="int")  # 1 if agent evaluated
 
     for i in range(N):
 
@@ -27,10 +39,18 @@ def SDT_gain(V=1, Vp=0, N=20, sigma=1, plot=True, get_gain=False):
         denom = p1 + p2
         probs[i] = p1 / denom
         gain[i] = V - (V * p1 + Vp * p2) / denom
+        default[i] = [V, Vp][switch[i]]
+        dynamic[i] = default[i]
+
+        if gain[i] > threshold:
+            dynamic[i] = V
+            did_eval[i] = 1
 
     if get_gain:
         return gain
-    elif plot:
+    elif get_dynamic:
+        return dynamic - default - (did_eval * cost_eval)
+    else:
         fig, ax1 = plt.subplots()
         fig.suptitle(f"V={V}, V'={Vp}, sigma={sigma}")
 
@@ -45,64 +65,56 @@ def SDT_gain(V=1, Vp=0, N=20, sigma=1, plot=True, get_gain=False):
         plt.show()
 
 
+def SDT_gain_fn(min_val, max_val, param, num=20, num_samples=1000, **kwargs):
+    vals = np.linspace(min_val, max_val, num=num)
+    gains = np.zeros(num)
+    for i, s in enumerate(vals):
+        kwargs.update({param: vals[i]})
+        gains[i] = sum(SDT_gain(**kwargs, N=num_samples)) / num_samples
+
+    return vals, gains
+
+
 SDT_gain()
 SDT_gain(V=0.1)
 SDT_gain(V=2)
 SDT_gain(V=11, Vp=10)
 SDT_gain(sigma=2)
 
-# average gain as a fn of sigma
-num = 20
-sigmas = np.linspace(0.1, 10, num=num)
-gains = np.zeros(num)
-for i, s in enumerate(sigmas):
-    num_samples = 1000
-    gains[i] = sum(SDT_gain(sigma=s, get_gain=True, N=num_samples)) / num_samples
-
+sigmas, gains = SDT_gain_fn(0.1, 10, "sigma", get_gain=True)
 plt.plot(sigmas, gains)
 plt.xlabel("Sigma")
 plt.ylabel("Average gain")
 plt.title("Average gain as a function of sigma for V=1, V'=0")
 plt.show()
 
-# average gain as a fn of V, fixing Vp at 0
-num = 50
-Vs = np.linspace(0.1, 10, num=num)
-gains = np.zeros(num)
-for i, v in enumerate(Vs):
-    num_samples = 10000
-    gains[i] = sum(SDT_gain(V=v, get_gain=True, N=num_samples)) / num_samples
+# average gain as a fn of V, fixing Vp at 0 and for 3 different values of sigma
+for s in [0.2, 1, 2]:
+    Vs, gains = SDT_gain_fn(
+        0.1, 10, "V", get_gain=True, num=50, sigma=s, num_samples=10000
+    )
+    plt.plot(Vs, gains)
+    plt.xlabel("V")
+    plt.ylabel("Average gain")
+    plt.title(f"Average gain as a function of V for sigma={s}, V'=0")
+    plt.show()
 
-plt.plot(Vs, gains)
-plt.xlabel("V")
-plt.ylabel("Average gain")
-plt.title("Average gain as a function of V for sigma=1, V'=0")
-plt.show()
-
-# average gain as a fn of V, fixing Vp at 0
-num = 50
-Vs = np.linspace(0.1, 10, num=num)
-gains = np.zeros(num)
-for i, v in enumerate(Vs):
-    num_samples = 10000
-    gains[i] = sum(SDT_gain(V=v, sigma=2, get_gain=True, N=num_samples)) / num_samples
-
-plt.plot(Vs, gains)
-plt.xlabel("V")
-plt.ylabel("Average gain")
-plt.title("Average gain as a function of V for sigma=2, V'=0")
-plt.show()
-
-# average gain as a fn of V, fixing Vp at 0
-num = 50
-Vs = np.linspace(0.1, 10, num=num)
-gains = np.zeros(num)
-for i, v in enumerate(Vs):
-    num_samples = 10000
-    gains[i] = sum(SDT_gain(V=v, sigma=0.2, get_gain=True, N=num_samples)) / num_samples
-
-plt.plot(Vs, gains)
-plt.xlabel("V")
-plt.ylabel("Average gain")
-plt.title("Average gain as a function of V for sigma=0.2, V'=0")
-plt.show()
+for cost_eval in [0.1, 0.2, 0.3, 0.4, 0.5]:
+    ts, rel_gains = SDT_gain_fn(
+        0,
+        0.5,
+        "threshold",
+        cost_eval=cost_eval,
+        num=20,
+        get_dynamic=True,
+        num_samples=10000,
+    )
+    plt.plot(ts, rel_gains)
+    plt.xlabel("Threshold")
+    plt.ylabel("Average difference in dynamic vs default value")
+    plt.title(
+        f"Average advantage of dynamic over default "
+        f"as a function of evaluation threshold for "
+        f"evaluation cost={cost_eval}, V=1, V'=0, sigma=1"
+    )
+    plt.show()
