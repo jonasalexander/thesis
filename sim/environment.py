500d2309544bf0decabc388749119d70768d0784
Jonas Nelle <jonas.a.nelle@gmail.com>
2020-08-11 12:57:21 +0200

    add calculate and plot gain for num_eval and Vb

--- sim/environment.py
+++ sim/environment.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from base import BaseGrid
-from decision_maker import DynamicDecisionMaker
+from decision_maker import DynamicDecisionMaker, DEFAULT_COST_EVAL
 
 
 class DecisionEnvironment:
@@ -14,7 +14,7 @@ class DecisionEnvironment:
 
         Parameters
         ----------
-        N : integer of numeric, optional
+        N : integer or numeric, optional
             The value of the actions. Defaults to 12.
         num_trials : int, optional
             The number of trials/equal decisions in the environment. Defaults
@@ -50,6 +50,85 @@ class DecisionEnvironment:
 
         # the value of the best action
         self.optimal = self.V.max(axis=1)
+
+    def calculate_gain(self, cost_eval=None):
+        """Calculate gain as a function of environmental variables.
+
+        Parameters
+        ----------
+        cost_eval : numeric, optional
+            The utility cost to evaluate an action. Defaults to DEFAULT_COST_EVAL.
+        """
+        cost_eval = cost_eval or DEFAULT_COST_EVAL
+
+        self.data = (
+            self.V.reset_index()
+            .rename(columns={"index": "trial"})
+            .melt(value_name="V", id_vars="trial", var_name="action index")
+            .sort_values(by=["trial", "action index"])
+            .reset_index(drop=True)
+        )
+
+        self.data["Vb"] = 0
+        for idx in self.data.index:
+            trial = self.data.loc[idx, "trial"]
+            V = self.data.loc[self.data["trial"] == trial, "V"]
+            for action_idx in V.index:
+                self.data.loc[idx, "Vb"] = max(V.loc[:idx])
+
+        self.data["gain"] = np.NaN
+        for trial in self.data["trial"].unique():
+            trial_idx = self.data["trial"] == trial
+            self.data.loc[trial_idx, "gain"] = np.roll(self.data.loc[trial_idx, "Vb"].diff(), shift=-1) - cost_eval
+
+    def plot_gain(self, metric, cost_eval=None):
+        """Plot gain (net utility) as a function of a specific environmental variable.
+
+
+        For continuous independent variables, use histogram for smooth
+        bucketing. For discrete, just take average (groupby with agg).
+
+        Parameters
+        ----------
+        metric : string
+            The environmental variable to use as the independent variable for
+            plotting.
+        cost_eval : numeric, optional
+            The utility cost to evaluate an action. Defaults to DEFAULT_COST_EVAL.
+        """
+
+        try:
+            self.data
+        except AttributeError as e:
+            self.calculate_gain(cost_eval)
+
+        if metric == "num_eval":
+            print("3")
+            plt.scatter(
+                range(self.N), self.data.groupby("action index").agg("mean")["gain"]
+            )
+            plt.plot(range(self.N), self.N * [0], color="r")
+            #plt.show()
+
+        elif metric == "Vb":
+            # bin by Vb and then for each bin, calculate mean gain
+
+            cutoffs = np.linspace(self.data["Vb"].min(), self.data["Vb"].max())
+            bins = pd.cut(self.data["Vb"], cutoffs)
+            Vb_values = self.data.groupby(bins)["gain"].agg("mean")
+
+            plt.figure(figsize=(20, 10))
+            plt.scatter(cutoffs[:-1], Vb_values.values)
+            plt.xticks(cutoffs[:-1:2])
+            plt.xlabel("value of best action so far")
+            plt.ylabel("net gain")
+            plt.show()
+
+        elif metric == "Vhat_next":
+            raise NotImplementedError(
+                "Vhat_next gain function plotting (and data tbh) not implemented yet."
+            )
+
 
 
 class DecisionEnvironmentGrid(BaseGrid):
