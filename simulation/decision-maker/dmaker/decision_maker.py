variance not stdev, fix off by one num_eval

simulation/decision-maker/dmaker/decision_maker.py
@@ -146,7 +146,7 @@ class DynamicDecisionMaker(BaseDecisionMaker):
             )
 
             # Agent's simulated distribution of V given Vhat, excluding cost_eval
-            cov_matrix = np.diag(np.repeat(self.env.sigma, self.env.N))
+            cov_matrix = np.diag(np.repeat(self.env.sigma ** 2, self.env.N))
             v_dist = np.random.multivariate_normal(Vhats, cov_matrix, self.num_samples)
 
             # Agent has to get the value of the first action
@@ -219,7 +219,7 @@ class DynamicDecisionMaker(BaseDecisionMaker):
                         Vhat_index = j
                         Vb = V_prev
 
-            self.data.loc[i, "num_eval"] = j + 1
+            self.data.loc[i, "num_eval"] = j
             self.data.loc[i, "utility"] = Vb - j * self.cost_eval
             self.data.loc[i, "V_index"] = V_index
             self.data.loc[i, "Vhat_index"] = Vhat_index

simulation/decision-maker/dmaker/environment.py
@@ -32,7 +32,7 @@ class DecisionEnvironment:
         self.N = N
         # each row of V (axis=1) is 1 trial
         unsorted_Vhat = np.random.multivariate_normal(
-            np.repeat(mu, N), np.diag(np.repeat(tau, N)), num_trials
+            np.repeat(mu, N), np.diag(np.repeat(tau ** 2, N)), num_trials
         )
         self.Vhat = pd.DataFrame(np.sort(unsorted_Vhat, 1)[:, ::-1])
         self.num_trials = num_trials
@@ -45,7 +45,7 @@ class DecisionEnvironment:
         # Draw Vh_i from V_i for each trial
         for action in self.Vhat.columns:
             self.V[action] = np.random.multivariate_normal(
-                self.Vhat[action], np.diag(np.repeat(sigma, num_trials))
+                self.Vhat[action], np.diag(np.repeat(sigma ** 2, num_trials))
             )
 
         # the value of the best action